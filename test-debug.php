b42ac96c36b8b99228976142c5240a0510c15df1
Martin Znamenáček <znamema1@fit.cvut.cz>
2017-04-22 16:33:11 +0200

    added debug method for PHP array printing

---
+++ test-debug.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+    header('Location: ../../');
+    exit;
+}
+
+function print_array_recursive($array, $shift) {
+    foreach ($array as $key => $value) {
+        echo $shift . "KEY: $key <br/>";
+        if (is_array($value)) {
+            print_array_recursive($value, $shift . '&nbsp&nbsp&nbsp');
+        } else {
+            echo $shift . "$value <br/>";
+        }
+    }
+}
